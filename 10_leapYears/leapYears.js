0b23bb4b93866e0e5347171812f6b2193f9b7933
mr cow <220004107+cowsareflying@users.noreply.github.com>
2025-07-27 14:02:07 +0000

    Complete exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+	const isDivisibleByFour = year % 4 === 0;
+	const isDivisibleByHundred = year % 100 === 0;
+	const isDivisibleByFourHundred = year % 400 === 0;
 
+	if (isDivisibleByFour && (!isDivisibleByHundred || isDivisibleByFourHundred)) {
+		return true;
+	}
+	return false;
 };
 
 // Do not edit below this line
